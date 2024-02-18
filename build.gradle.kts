a0a3aff1cb61b89ddded976e52f47e31060e26a6
Todd Ericskon <todd.erickson87@gmail.com>
2024-02-17 17:30:18 -0700

    Added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,17 @@
 
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val ktormVersion: String by project
+val postgresqlDriverVersion: String by project
+val kotestVersion: String by project
+val mockkVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.9.22"
     id("io.ktor.plugin") version "2.3.8"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+    jacoco
 }
 
 group = "io.github.terickson87"
@@ -32,7 +37,29 @@ dependencies {
     implementation("io.ktor:ktor-server-metrics-jvm")
     implementation("io.ktor:ktor-server-call-id-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    // Testing
     testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    // Kotest
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+    // MockK
+    testImplementation("io.mockk:mockk:$mockkVersion")
+
+    // DB
+    implementation("org.ktorm:ktorm-core:$ktormVersion")
+    implementation("org.ktorm:ktorm-support-postgresql:$ktormVersion")
+    implementation("org.postgresql:postgresql:$postgresqlDriverVersion")
+}
+
+tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
