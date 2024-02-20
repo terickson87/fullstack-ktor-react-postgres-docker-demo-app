7fd98400821774f47c07c70d732d13962387c7a3
Todd Ericskon <todd.erickson87@gmail.com>
2024-02-20 11:36:27 -0700

    Adjustment to docker build and clean up

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM gradle:8.6-jdk21-alpine as build
 COPY --chown=gradle:gradle . /app
 WORKDIR /app
-RUN gradle buildFatJar --no-daemon
+RUN ./gradlew buildFatJar --no-daemon
 
 FROM amazoncorretto:21-alpine3.19
 COPY --from=build /app/build/libs/*-all.jar /bin/app.jar
--- sql/init-db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE notes IF NOT EXISTS
-(
-  id SERIAL NOT NULL,
-  created_at timestamp default current_timestamp,
-  body varchar NOT NULL
-);
-INSERT INTO notes(body) VALUES ('Today I planted some Strawberries.');
-INSERT INTO notes(body) VALUES ('Today I harvested two Strawberries.');
