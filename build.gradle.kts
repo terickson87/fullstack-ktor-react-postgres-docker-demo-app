3a245805262097dfffe122aa156bd443a784da6e
Todd Ericskon <todd.erickson87@gmail.com>
2024-02-21 11:39:25 -0700

    Fixed a IDEA testing problem with the build

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,13 +63,12 @@ dependencies {
     implementation("org.postgresql:postgresql:$postgresqlDriverVersion")
 }
 
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
-}
-
 tasks.test {
     finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
 }
 
 ktor {
