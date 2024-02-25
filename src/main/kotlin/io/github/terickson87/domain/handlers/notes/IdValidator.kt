dddf5ec4f0858e17a427767c18e67d892dd430ba
Todd Ericskon <todd.erickson87@gmail.com>
2024-02-25 10:46:20 -0700

    Created CreateNote and GetNoteById handlers

---
+++ src/main/kotlin/io/github/terickson87/domain/handlers/notes/IdValidator.kt
@@ -0,0 +1,41 @@
+package io.github.terickson87.domain.handlers.notes
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+
+class IdValidator {
+    data class Input(val id: String?)
+
+    sealed class Output {
+        data class Success(val id: Int) : Output()
+        data object MissingID : Output()
+        data class BadId(val id: String) : Output()
+    }
+
+    companion object {
+        fun validate(input: Input): Output =
+            input.id?.let { validateNonNullId(it) }
+                ?: Output.MissingID
+
+        private fun validateNonNullId(id: String): Output {
+            return try {
+                id.toInt().let { Output.Success(it) }
+            } catch (e: NumberFormatException) {
+                return Output.BadId(id)
+            }
+        }
+        suspend inline fun ApplicationCall.validateCallId(block:(id :Int) -> Unit) =
+            this.parameters["id"]
+                ?.let { Input(it) }
+                ?.let { validate(it) }
+                ?.let {
+                    when(it) {
+                        is Output.BadId ->
+                            this.respondText("ID '${it.id}' is not an integer", status = HttpStatusCode.BadRequest)
+                        is Output.MissingID -> this.respondText("Missing ID", status = HttpStatusCode.BadRequest)
+                        is Output.Success -> block(it.id)
+                    }
+                }
+    }
+}
---
+++ src/test/kotlin/io/github/terickson87/domain/handlers/notes/GetNoteByIdHandlerTest.kt
@@ -0,0 +1,50 @@
+package io.github.terickson87.domain.handlers.notes
+
+import io.github.terickson87.adapter.accessor.NotesAccessor
+import io.github.terickson87.domain.Note
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeTypeOf
+import io.mockk.every
+import io.mockk.mockk
+import java.time.Instant
+
+class GetNoteByIdHandlerTest : BehaviorSpec ({
+
+    val notesAccessorMock: NotesAccessor = mockk()
+    val getNotesByIdHandler = GetNoteByIdHandler(notesAccessorMock)
+
+    val noteNow = Instant.now()
+    val testNote = Note(17, noteNow, noteNow, "test Note Body")
+
+    given("Integer id that is in the database") {
+        val input = GetNoteByIdHandler.Input(testNote.id)
+        every { notesAccessorMock.getNoteById(testNote.id) } returns testNote
+
+        `when`("handle") {
+            val result = getNotesByIdHandler.handle(input)
+
+            then("returns Output.MissingID")
+            result.shouldBeTypeOf<GetNoteByIdHandler.Output.Success>().noteResponse.also {
+                it.id.shouldBe(testNote.id)
+                it.createdAt.shouldBe(noteNow)
+                it.modifiedAt.shouldBe(noteNow)
+                it.body.shouldBe(testNote.body)
+            }
+        }
+    }
+
+    given("Integer id that is NOT in the database") {
+        val input = GetNoteByIdHandler.Input(testNote.id)
+        every { notesAccessorMock.getNoteById(testNote.id) } returns null
+
+        `when`("handle") {
+            val result = getNotesByIdHandler.handle(input)
+
+            then("returns Output.MissingID")
+            result.shouldBeTypeOf<GetNoteByIdHandler.Output.IdNotFound>().also {
+                it.id.shouldBe(testNote.id)
+            }
+        }
+    }
+})
