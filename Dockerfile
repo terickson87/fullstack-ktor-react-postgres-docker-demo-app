587baeb4bee73b79a68599606973351c5422460c
Todd Ericskon <todd.erickson87@gmail.com>
2024-03-03 21:21:05 -0700

    Added TODOs

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,7 @@ FROM amazoncorretto:21-alpine3.19
 COPY --from=build /app/build/libs/*-all.jar /bin/app.jar
 WORKDIR /bin
 CMD ["java","-jar","app.jar"]
+
+  # TODO Add ENV vars in a .env file
+  # pull same values in application.conf https://ktor.io/docs/configuration-file.html#environment-variables
+  # and in Kotlin files https://stackoverflow.com/a/48104838/2561852
--- src/main/kotlin/io/github/terickson87/croprecordsservice/adapter/PostgresSingleton.kt
+++ src/main/kotlin/io/github/terickson87/croprecordsservice/adapter/PostgresSingleton.kt
@@ -8,6 +8,11 @@ object PostgresSingleton {
 
     private lateinit var database: Database
 
+    // TODO Add in HikariCP https://github.com/brettwooldridge/HikariCP
+    // TODO add Flyway https://proandroiddev.com/how-to-deploy-a-ktor-server-using-docker-postgresql-and-flyway-a-journey-to-effortless-24464b0752de
+    //  & https://github.com/limadelrey/vertx-4-reactive-rest-api/blob/master/src/main/resources/db/migration/V1__initial_schema.sql
+    // TODO add adminer
+
     fun init() {
         database = Database.connect(
             url = "jdbc:postgresql://postgres:5432/postgres",
