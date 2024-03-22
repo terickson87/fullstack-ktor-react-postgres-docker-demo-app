795a7ad3a4e40b09a7d7a2b6e3bf475d94433017
Todd Ericskon <todd.erickson87@gmail.com>
2024-03-21 18:07:05 -0700

    removed periodic metrics

--- src/main/kotlin/io/github/terickson87/ktorpostgresdemo/plugins/Monitoring.kt
+++ src/main/kotlin/io/github/terickson87/ktorpostgresdemo/plugins/Monitoring.kt
@@ -1,14 +1,14 @@
 package io.github.terickson87.ktorpostgresdemo.plugins
 
-import com.codahale.metrics.*
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.metrics.dropwizard.*
 import io.ktor.server.plugins.callid.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.request.*
-import java.util.concurrent.TimeUnit
 import org.slf4j.event.*
+//import com.codahale.metrics.*
+//import io.ktor.server.metrics.dropwizard.*
+//import java.util.concurrent.TimeUnit
 
 fun Application.configureMonitoring() {
     install(CallLogging) {
@@ -16,14 +16,14 @@ fun Application.configureMonitoring() {
         filter { call -> call.request.path().startsWith("/") }
         callIdMdc("call-id")
     }
-    install(DropwizardMetrics) {
-        Slf4jReporter.forRegistry(registry)
-            .outputTo(this@configureMonitoring.log)
-            .convertRatesTo(TimeUnit.SECONDS)
-            .convertDurationsTo(TimeUnit.MILLISECONDS)
-            .build()
-            .start(10, TimeUnit.SECONDS)
-    }
+    // install(DropwizardMetrics) {
+    //     Slf4jReporter.forRegistry(registry)
+    //         .outputTo(this@configureMonitoring.log)
+    //         .convertRatesTo(TimeUnit.SECONDS)
+    //         .convertDurationsTo(TimeUnit.MILLISECONDS)
+    //         .build()
+    //         .start(10, TimeUnit.SECONDS)
+    // }
     install(CallId) {
         header(HttpHeaders.XRequestId)
         verify { callId: String ->
