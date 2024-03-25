656b069d1bb19f2b3a953b674840db0bf0b85557
Todd Ericskon <todd.erickson87@gmail.com>
2024-03-25 08:17:30 -0700

    Made it so the frontend gets served

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
-FROM gradle:8.6-jdk21-alpine as build
+FROM gradle:8.6-jdk21-alpine as BackendBuild
 COPY --chown=gradle:gradle . /app
 WORKDIR /app
 RUN gradle buildFatJar --no-daemon
 
+FROM node:21-alpine3.19 as FrontendBuild
+COPY fullstack-frontend-demo-react-vite /website
+WORKDIR /website
+RUN npm ci && npm run build
+
 FROM amazoncorretto:21-alpine3.19
-COPY --from=build /app/build/libs/*-all.jar /bin/app.jar
+COPY --from=FrontendBuild /website/dist /bin/website
+COPY --from=BackendBuild /app/build/libs/*-all.jar /bin/app.jar
 WORKDIR /bin
 CMD ["java","-jar","app.jar"]
 
--- src/main/kotlin/io/github/terickson87/ktorpostgresdemo/plugins/Routing.kt
+++ src/main/kotlin/io/github/terickson87/ktorpostgresdemo/plugins/Routing.kt
@@ -3,11 +3,16 @@ package io.github.terickson87.ktorpostgresdemo.plugins
 import io.github.terickson87.ktorpostgresdemo.adapter.accessor.NotesAccessor
 import io.github.terickson87.ktorpostgresdemo.routing.NoteRouting
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting(notesAccessor: NotesAccessor) {
     routing {
         val noteRouting = NoteRouting(notesAccessor)
         noteRouting.noteRouting(this)
+        singlePageApplication {
+            react("website")
+            // you need to copy the files over because they aren't being copied to the docker container
+        }
     }
 }
