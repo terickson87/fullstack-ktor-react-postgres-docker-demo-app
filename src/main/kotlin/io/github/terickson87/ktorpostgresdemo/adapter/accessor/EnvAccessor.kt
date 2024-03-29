3aa3dd044a670a775476e25ba8dd88d81220fe95
Todd Ericskon <todd.erickson87@gmail.com>
2024-03-29 12:02:46 -0700

    Removed getIsFrontEndDev() from EnvAccessor

--- src/main/kotlin/io/github/terickson87/ktorpostgresdemo/adapter/accessor/EnvAccessor.kt
+++ src/main/kotlin/io/github/terickson87/ktorpostgresdemo/adapter/accessor/EnvAccessor.kt
@@ -2,5 +2,4 @@ package io.github.terickson87.ktorpostgresdemo.adapter.accessor
 
 interface EnvAccessor {
     fun getDbHost(): String
-    fun getIsFrontEndDev(): Boolean
 }
--- src/main/kotlin/io/github/terickson87/ktorpostgresdemo/adapter/accessor/SystemEnvAccessor.kt
+++ src/main/kotlin/io/github/terickson87/ktorpostgresdemo/adapter/accessor/SystemEnvAccessor.kt
@@ -1,12 +1,16 @@
 package io.github.terickson87.ktorpostgresdemo.adapter.accessor
 
-// In case this need to be mocked: https://notwoods.github.io/mockk-guidebook/docs/mocking/static/
+/*
+In case this need to be mocked: https://notwoods.github.io/mockk-guidebook/docs/mocking/static/
+Env Vars with `docker compose`:
+https://docs.docker.com/compose/environment-variables/set-environment-variables/#set-environment-variables-with-docker-compose-run---env
+
+With reference to Ktor explicitly:
+https://github.com/sharpmind-de/ktor-env-config?tab=readme-ov-file#java--jar
+*/
 object SystemEnvAccessor : EnvAccessor {
 
     private const val DB_HOST_ENV_VAR = "DB_HOST"
     private const val DEFAULT_DB_HOST = "127.0.0.1:5432"
-    private const val IS_FRONT_END_DEV_ENV_VAR = "IS_FRONT_END_DEV"
     override fun getDbHost(): String = System.getenv(DB_HOST_ENV_VAR) ?: DEFAULT_DB_HOST
-
-    override fun getIsFrontEndDev(): Boolean = System.getenv(IS_FRONT_END_DEV_ENV_VAR).toBoolean()
 }
